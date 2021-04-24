3955a4e4868c93a5c020fdd809369f742bb1cf65
Raul Jimenez <elecash@gmail.com>
2021-04-24 19:12:47 +0200

    Fix export card in export tool and wild horizons set

--- apps/card-builder/src/app/export-tool/export-tool.component.ts
+++ apps/card-builder/src/app/export-tool/export-tool.component.ts
@@ -203,13 +203,13 @@ export class ExportToolComponent {
 
         if (this.cardData.type === 'BATTLEFIELD') {
             options = {
-                width: 1000,
-                height: 705,
+                width: 1070,
+                height: 775,
             };
         } else {
             options = {
-                width: 705,
-                height: 1000,
+                width: 775,
+                height: 1070,
             };
         }
         window.scrollTo(0, 0);
